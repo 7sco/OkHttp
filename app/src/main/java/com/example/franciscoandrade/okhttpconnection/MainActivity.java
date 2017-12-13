cf39975e568fafb9ac148163ea3d908f5c75561b
7sco <xaviandrade14@gmail.com>
2017-12-12 21:19:40 -0500

    OkHTTP App

---
+++ app/src/main/java/com/example/franciscoandrade/okhttpconnection/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.franciscoandrade.okhttpconnection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    private void makeRequestWithOkHttp(String url) {
+        OkHttpClient client = new OkHttpClient();   // 1
+        Request request = new Request.Builder().url(url).build();  // 2
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) { // 3
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                final String result = response.body().string();  // 4
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // perform some ui work with `result`  // 5
+                            TextView tv = (TextView) findViewById(R.id.text_view);
+                            tv.setText(result);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    public void onCLick(View view) {
+
+
+        makeRequestWithOkHttp("https://swapi.co/api/people/1");
+
+    }
+}
